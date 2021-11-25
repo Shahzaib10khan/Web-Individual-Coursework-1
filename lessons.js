3330669c1c476a12e932a7bdb0077d0de1394d4e
Shahzaib10khan <shah.clifton2021@gmail.com>
2021-11-25 22:47:46 +0500

    lessons added

---
+++ lessons.js
@@ -0,0 +1,102 @@
+const lessons = [
+    {
+        "id": 1,
+        "course": "C++",
+        "thumbnail": "https://www.onlevelup.com/wp-content/uploads/2021/03/logo-c-for-quiz.jpg",
+        "description": "C++ is a cross-platform language that can be used to create high-performance applications.",
+        "city": 'Islamabad',
+        "price": 15000,
+        "space": 4,
+        "reserved": 0
+    },
+    {
+        "id": 2,
+        "course": "Java",
+        "thumbnail": "https://latestlyhunt.com/wp-content/uploads/2021/04/Java-Kya-Hai-Know-Java-Ka-Full-Form-and-its.jpeg",
+        "description": "Java is a high-level, class-based, object-oriented programming language.",
+        "city": "Peshawar",
+        "price": 17000,
+        "space": 5,
+        "reserved": 0
+    },
+    {
+        "id": 3,
+        "course": "Python",
+        "thumbnail": "https://www.freecodecamp.org/news/content/images/2020/05/Python-language.png",
+        "description": "Python is an interpreted high-level general-purpose programming language.",
+        "city": 'Rawalpindi',
+        "price": 7800,
+        "space": 2,
+        "reserved": 0
+    },
+    {
+        "id": 4,
+        "course": "PHP",
+        "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png",
+        "description": "PHP is a popular general-purpose scripting language that is especially suited to web development.",
+        "city": 'Lahor',
+        "price": 5600,
+        "space": 3,
+        "reserved": 0
+    },
+    {
+        "id": 5,
+        "course": "Javascript",
+        "thumbnail": "https://www.w3schools.com/whatis/img_js.png",
+        "description": "JavaScript is the world's most popular programming language. JavaScript is the programming language of the Web.",
+        "city": 'Peshawar',
+        "price": 7600,
+        "space": 0,
+        "reserved": 0
+    },
+    {
+        "id": 6,
+        "course": "Vue.js",
+        "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png",
+        "description": "Vue.js is an open-source front end JavaScript framework for building user interfaces and single-page applications.",
+        "city": 'Karachi',
+        "price": 12000,
+        "space": 1,
+        "reserved": 0
+    },
+    {
+        "id": 7,
+        "course": "Angular.js",
+        "thumbnail": "https://iotvnaw69daj.i.optimole.com/AXVzL2w.n2y9~6666f/w:auto/h:auto/q:90/https://www.codeinwp.com/wp-content/uploads/2019/01/angular-logo.png",
+        "description": "Angular is a platform for building mobile and desktop web applications. ",
+        "city": 'Quetta',
+        "price": 8300,
+        "space": 5,
+        "reserved": 0
+    },
+    {
+        "id": 8,
+        "course": "React.js",
+        "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+        "description": "React · Declarative. React makes it painless to create interactive UIs. · Component-Based.",
+        "city": 'Faisalabad',
+        "price": 8600,
+        "space": 5,
+        "reserved": 0
+    },
+    {
+        "id": 9,
+        "course": "Unity",
+        "thumbnail": "https://mpng.subpng.com/20180811/sgg/kisspng-brand-unity-5b6eea4aaa2ed0.5639891215339955946971.jpg",
+        "description": "Unity is a cross-platform game engine with a built-in IDE developed by Unity Technologies",
+        "city": 'Multan',
+        "price": 9000,
+        "space": 5,
+        "reserved": 0
+    },
+    {
+        "id": 10,
+        "course": "C#",
+        "thumbnail": "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png",
+        "description": "C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework. ",
+        "city": 'Hyderabad',
+        "price": 8000,
+        "space": 4,
+        "reserved": 0
+    }
+]
