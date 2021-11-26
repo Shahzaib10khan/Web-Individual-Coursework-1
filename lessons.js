b7a3e15e478c060e7db6f4eab699cfe486a09e56
Shahzaib10khan <shah.clifton2021@gmail.com>
2021-11-26 19:07:14 +0500

    final commit

--- lessons.js
+++ lessons.js
@@ -4,9 +4,9 @@ const lessons = [
         "course": "C++",
         "thumbnail": "https://www.onlevelup.com/wp-content/uploads/2021/03/logo-c-for-quiz.jpg",
         "description": "C++ is a cross-platform language that can be used to create high-performance applications.",
-        "city": 'Islamabad',
+        "location": 'Islamabad',
         "price": 15000,
-        "space": 4,
+        "space": 5,
         "reserved": 0
     },
     {
@@ -14,7 +14,7 @@ const lessons = [
         "course": "Java",
         "thumbnail": "https://latestlyhunt.com/wp-content/uploads/2021/04/Java-Kya-Hai-Know-Java-Ka-Full-Form-and-its.jpeg",
         "description": "Java is a high-level, class-based, object-oriented programming language.",
-        "city": "Peshawar",
+        "location": "Peshawar",
         "price": 17000,
         "space": 5,
         "reserved": 0
@@ -24,9 +24,9 @@ const lessons = [
         "course": "Python",
         "thumbnail": "https://www.freecodecamp.org/news/content/images/2020/05/Python-language.png",
         "description": "Python is an interpreted high-level general-purpose programming language.",
-        "city": 'Rawalpindi',
+        "location": 'Rawalpindi',
         "price": 7800,
-        "space": 2,
+        "space": 5,
         "reserved": 0
     },
     {
@@ -34,9 +34,9 @@ const lessons = [
         "course": "PHP",
         "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png",
         "description": "PHP is a popular general-purpose scripting language that is especially suited to web development.",
-        "city": 'Lahor',
+        "location": 'Lahor',
         "price": 5600,
-        "space": 3,
+        "space": 5,
         "reserved": 0
     },
     {
@@ -44,9 +44,9 @@ const lessons = [
         "course": "Javascript",
         "thumbnail": "https://www.w3schools.com/whatis/img_js.png",
         "description": "JavaScript is the world's most popular programming language. JavaScript is the programming language of the Web.",
-        "city": 'Peshawar',
+        "location": 'Peshawar',
         "price": 7600,
-        "space": 0,
+        "space": 5,
         "reserved": 0
     },
     {
@@ -54,9 +54,9 @@ const lessons = [
         "course": "Vue.js",
         "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png",
         "description": "Vue.js is an open-source front end JavaScript framework for building user interfaces and single-page applications.",
-        "city": 'Karachi',
+        "location": 'Karachi',
         "price": 12000,
-        "space": 1,
+        "space": 5,
         "reserved": 0
     },
     {
@@ -64,7 +64,7 @@ const lessons = [
         "course": "Angular.js",
         "thumbnail": "https://iotvnaw69daj.i.optimole.com/AXVzL2w.n2y9~6666f/w:auto/h:auto/q:90/https://www.codeinwp.com/wp-content/uploads/2019/01/angular-logo.png",
         "description": "Angular is a platform for building mobile and desktop web applications. ",
-        "city": 'Quetta',
+        "location": 'Quetta',
         "price": 8300,
         "space": 5,
         "reserved": 0
@@ -74,7 +74,7 @@ const lessons = [
         "course": "React.js",
         "thumbnail": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
         "description": "React · Declarative. React makes it painless to create interactive UIs. · Component-Based.",
-        "city": 'Faisalabad',
+        "location": 'Faisalabad',
         "price": 8600,
         "space": 5,
         "reserved": 0
@@ -84,7 +84,7 @@ const lessons = [
         "course": "Unity",
         "thumbnail": "https://mpng.subpng.com/20180811/sgg/kisspng-brand-unity-5b6eea4aaa2ed0.5639891215339955946971.jpg",
         "description": "Unity is a cross-platform game engine with a built-in IDE developed by Unity Technologies",
-        "city": 'Multan',
+        "location": 'Multan',
         "price": 9000,
         "space": 5,
         "reserved": 0
@@ -94,9 +94,9 @@ const lessons = [
         "course": "C#",
         "thumbnail": "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png",
         "description": "C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework. ",
-        "city": 'Hyderabad',
+        "location": 'Hyderabad',
         "price": 8000,
-        "space": 4,
+        "space": 5,
         "reserved": 0
     }
 ]
